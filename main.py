8f0ba005be4e5fce84ba9e7c8c63099ba8698477
Temirov Nurbek <nurbektemirov073@gmail.com>
2024-07-07 23:13:50 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("Bazadan uchirish")
+    await create_tables()
+    print("Malumotlar bazasi tayyor")
+    yield
+    print("O'chirish")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+
---
+++ router.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from repositery import TaskRepository
+from schemas import STaskAdd, STask, STaskId
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["Tags"],
+)
+
+
+@router.post('')
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+) -> STaskId:
+    task_id = await TaskRepository.add_one(task)
+    return {"Ok": True, 'task_id': task_id}
+
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.find_all()
+    return tasks
